17afefb6c845241d3a2773d94152bf42e7c632f2
David Mikuláštík <mikulastikd@gmail.com>
2020-10-22 17:09:12 +0200

    feat: bullet train http client

---
+++ BulletTrainHttpClient.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace BulletTrain
+{
+    public class BulletTrainHttpClient : IBulletTrainHttpClient
+    {
+        private readonly HttpClient _client = new HttpClient();
+        private readonly BulletTrainConfiguration _configuration;
+
+        public BulletTrainHttpClient(BulletTrainConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<TResponse> GetAsync<TResponse>(Uri uri)
+        {
+            var response = await GetJsonAsync(HttpMethod.Get, uri);
+
+            return JsonSerializer.Deserialize<TResponse>(response);
+        }
+
+        public async Task<TResponse> PostAsync<TResponse>(Uri uri, object payload)
+        {
+            var response = await GetJsonAsync(HttpMethod.Post, uri, JsonSerializer.Serialize(payload));
+
+            return JsonSerializer.Deserialize<TResponse>(response);
+        }
+
+        private async Task<string> GetJsonAsync(HttpMethod method, Uri url, string body = null)
+        {
+            var request = new HttpRequestMessage(method, url)
+            {
+                Headers =
+                {
+                    {"X-Environment-Key", _configuration.EnvironmentKey}
+                }
+            };
+            if (body != null)
+            {
+                request.Content = new StringContent(body, Encoding.UTF8, "application/json");
+            }
+
+            var response = await _client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
---
+++ IBulletTrainHttpClient.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace BulletTrain
+{
+    public interface IBulletTrainHttpClient
+    {
+        Task<TResponse> GetAsync<TResponse>(Uri uri);
+        Task<TResponse> PostAsync<TResponse>(Uri uri, object payload);
+    }
+}
