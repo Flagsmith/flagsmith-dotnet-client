61e5fc21ed2b94db2931ab1c07b09e7b583ff4b5
FOLIO3\asadabbas <asadabbas@folio3.com>
2022-02-23 20:26:32 +0500

    updated the environmentrefresh test to check for atleast 3 times.

--- Flagsmith.Client.Test/PollingManagerTest.cs
+++ Flagsmith.Client.Test/PollingManagerTest.cs
@@ -35,7 +35,7 @@ namespace Flagsmith.FlagsmithClientTest
             var x = new PollingManager(callback, 1);
             _ = x.StartPoll();
             await Task.Delay(3000);
-            Assert.Equal(3, calledCount);
+            Assert.True(calledCount >= 3); // check pollingmanager polls atlease 3 times.
             x.StopPoll();
         }
     }
