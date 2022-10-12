df53e0185b5cbd915033ec12bb8b9d9bd710b9ee
Radek Matejka <e-Radek.Matejka@embedit.cz>
2022-10-12 08:49:20 +0200

    fix: client unit tests and "config" From() => constructor

--- Flagsmith.FlagsmithClient/FlagsmithConfiguration.cs
+++ Flagsmith.FlagsmithClient/FlagsmithConfiguration.cs
@@ -11,22 +11,22 @@ namespace Flagsmith
         /// <summary>
         /// Override the URL of the Flagsmith API to communicate with.
         /// </summary>
-        public string ApiUrl { get; set; } = DefaultApiUrl;
+        public string ApiUrl { get; set; }
 
         /// <summary>
         /// The environment key obtained from Flagsmith interface.
         /// </summary>
-        public string EnvironmentKey { get; set; } = string.Empty;
-
-        /// <summary>
-        /// If using local evaluation, specify the interval period between refreshes of local environment data.
-        /// </summary>
-        public int EnvironmentRefreshIntervalSeconds { get; set; } = 60;
+        public string EnvironmentKey { get; set; }
 
         /// <summary>
         /// Enables local evaluation of flags.
         /// </summary>
         public bool EnableClientSideEvaluation { get; set; }
+
+        /// <summary>
+        /// If using local evaluation, specify the interval period between refreshes of local environment data.
+        /// </summary>
+        public int EnvironmentRefreshIntervalSeconds { get; set; }
 
         /// <summary>
         /// Callable which will be used in the case where flags cannot be retrieved from the API or a non existent feature is requested.
@@ -55,27 +55,34 @@ namespace Flagsmith
 
         IReadOnlyDictionary<string, string> IFlagsmithClientConfig.CustomHeaders => CustomHeaders;
 
-        public static FlagsmithConfiguration From(
+        public FlagsmithConfiguration()
+        {
+            ApiUrl = DefaultApiUrl;
+            EnvironmentKey = string.Empty;
+            EnvironmentRefreshIntervalSeconds = 60;
+        }
+
+        public FlagsmithConfiguration(
             string environmentKey,
             string apiUrl = DefaultApiUrl,
             Func<string, IFlag> defaultFlagHandler = null,
             bool enableAnalytics = false,
+            bool enableClientSideEvaluation = false,
             int environmentRefreshIntervalSeconds = 60,
             Dictionary<string, string> customHeaders = null,
             int? retries = null,
             double? requestTimeout = null)
+            : this()
         {
-            return new FlagsmithConfiguration
-            {
-                EnvironmentKey = environmentKey,
-                ApiUrl = apiUrl,
-                DefaultFlagHandler = defaultFlagHandler,
-                EnableAnalytics = enableAnalytics,
-                EnvironmentRefreshIntervalSeconds = environmentRefreshIntervalSeconds,
-                CustomHeaders = customHeaders,
-                Retries = retries,
-                RequestTimeout = requestTimeout,
-            };
+            EnvironmentKey = environmentKey;
+            ApiUrl = apiUrl;
+            DefaultFlagHandler = defaultFlagHandler;
+            EnableAnalytics = enableAnalytics;
+            EnableClientSideEvaluation = enableClientSideEvaluation;
+            EnvironmentRefreshIntervalSeconds = environmentRefreshIntervalSeconds;
+            CustomHeaders = customHeaders;
+            Retries = retries;
+            RequestTimeout = requestTimeout;
         }
     }
 }
