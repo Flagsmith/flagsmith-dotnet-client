da0505963ed6e5c6ee8c5333de1780bb8a3d07af
Matthew Elwell <matthew.elwell@flagsmith.com>
2025-03-13 11:46:33 +0000

    Throw exception when trying to instantiate client in local eval without server key

--- Flagsmith.Client.Test/FlagsmithTest.cs
+++ Flagsmith.Client.Test/FlagsmithTest.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -520,6 +520,24 @@ namespace Flagsmith.FlagsmithClientTest
 
             // Then
             var exception = Assert.Throws<Exception>(() => createFlagsmith());
+            Assert.Equal
+            (
+                "ValueError: In order to use local evaluation, please generate a server key in the environment settings page.",
+                exception.Message
+            );
+        }
+
+        [Fact]
+        public void TestCannotCreateFlagsmithClientInLocalEvaluationWithoutServerAPIKey()
+        {
+            // When
+            Action createFlagsmith = () => new FlagsmithClient(
+                environmentKey: "foobar",
+                enableClientSideEvaluation: true
+            );
+
+            // Then
+            var exception = Assert.Throws<Exception>(() => createFlagsmith());
             Assert.Equal("ValueError: environmentKey is required", exception.Message);
         }
 
@@ -554,7 +572,7 @@ namespace Flagsmith.FlagsmithClientTest
                 featuresDictionary.TryAdd($"Feature_{i}", 0);
             }
 
-            // When 
+            // When
             var tasks = new Task[numberOfThreads];
 
             // Create numberOfThreads threads.
@@ -566,13 +584,13 @@ namespace Flagsmith.FlagsmithClientTest
                 // Prepare an array of feature names of length callsPerThread.
                 for (int j = 0; j < callsPerThread; j++)
                 {
-                    // The feature names are randomly selected from the featuresDictionary and added to the 
-                    // list of features, which represents the features that have been evaluated.  
+                    // The feature names are randomly selected from the featuresDictionary and added to the
+                    // list of features, which represents the features that have been evaluated.
                     string featureName = $"Feature_{new Random().Next(1, featuresDictionary.Count + 1)}";
                     features[j] = featureName;
 
                     // The relevant key in the featuresDictionary is incremented to simulate an evaluation
-                    // to track for that feature. 
+                    // to track for that feature.
                     featuresDictionary[featureName]++;
                 }
 
